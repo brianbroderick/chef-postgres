f62966368e2b1eb7db51015baf6c190810c50e9b
brianbroderick <brianbroderick19@gmail.com>
2017-11-01 14:59:02 -0600

    Revert "Add asdf to root user"
    This reverts commit 7d3d6c29a3941345cf64a799bee563d51442c1dd.

--- asdf/recipes/install_versions.rb
+++ asdf/recipes/install_versions.rb
@@ -7,20 +7,10 @@ bash "install_erlang" do
   notifies :run, "ruby_block[install_erlang]", :before      
 end
 
-bash "root_install_erlang" do  
-  code "#{node["asdf"]["root_asdf_location"]} install erlang 20.0"
-  notifies :run, "ruby_block[install_erlang]", :before      
-end
-
 bash "install_elixir" do 
   environment ({ 'HOME' => ::Dir.home("ubuntu"), 'USER' => "ubuntu" })
   user "ubuntu" 
   code "#{node["asdf"]["asdf_location"]} install elixir 1.5.1"
-  notifies :run, "ruby_block[install_elixir]", :before      
-end
-
-bash "root_install_elixir" do 
-  code "#{node["asdf"]["root_asdf_location"]} install elixir 1.5.1"
   notifies :run, "ruby_block[install_elixir]", :before      
 end
 
@@ -31,19 +21,9 @@ bash "install_ruby" do
   notifies :run, "ruby_block[install_ruby]", :before       
 end
 
-bash "root_install_ruby" do  
-  code "#{node["asdf"]["root_asdf_location"]} install ruby 2.4.2"
-  notifies :run, "ruby_block[install_ruby]", :before       
-end
-
 bash "install_hex" do 
   environment ({ 'HOME' => ::Dir.home("ubuntu"), 'USER' => "ubuntu" })
   user "ubuntu"
   code "/home/ubuntu/.asdf/shims/mix local.hex --force"
-  notifies :run, "ruby_block[install_hex]", :before
-end
-
-bash "root_install_hex" do 
-  code "/root/.asdf/shims/mix local.hex --force"
-  notifies :run, "ruby_block[install_hex]", :before
+  notifies :run, "ruby_block[install_ruby]", :before
 end
