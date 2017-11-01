772d184325eb7f8a942c9f69ab7e441e88d19033
brianbroderick <brianbroderick19@gmail.com>
2017-11-01 14:58:58 -0600

    Revert "Debugging hex"
    This reverts commit 5f4ebf8ee0dd7c7fcefc915fa70891a6864e17c0.

--- asdf/recipes/install_elixir_tools.rb
+++ asdf/recipes/install_elixir_tools.rb
@@ -3,13 +3,11 @@ include_recipe "asdf::log_output"
 bash "install_hex" do 
   environment ({ 'HOME' => ::Dir.home("ubuntu"), 'USER' => "ubuntu" })
   user "ubuntu"
-  code ". $HOME/.asdf/asdf.sh
-  /home/ubuntu/.asdf/shims/mix local.hex --force"
+  code "/home/ubuntu/.asdf/shims/mix local.hex --force"
   notifies :run, "ruby_block[install_hex]", :before
 end
 
 bash "root_install_hex" do 
-  code ". $HOME/.asdf/asdf.sh
-  /root/.asdf/shims/mix local.hex --force"
+  code "/root/.asdf/shims/mix local.hex --force"
   notifies :run, "ruby_block[install_hex]", :before
 end
